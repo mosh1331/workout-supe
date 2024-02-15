b4b04f5e719600d6ffe4a942bb1bad9a5bdb1055
developer-tvc <developer@techversantinfo.com>
2024-02-15 11:18:07 +0530

    style change

--- src/components/ViewScreen.jsx
+++ src/components/ViewScreen.jsx
@@ -67,24 +67,24 @@ const ViewScreen = () => {
     return <div>{error}</div>
   }
 
-  console.log(workoutDays, 'workoutDays')
   return (
     <div className="text-3xl font-bold min-h-[100vh] w-full relative pt-[40px]" >
       <div className="flex flex-row w-full justify-between">
         {days.map((date, index) => {
+          const currentDate = new Date()
           return(
-            <div key={index} className={`w-[15%] m-2 text-center bg-[white] border-2 rounded relative `}>
+            <div key={index} className={`w-[15%] m-2 text-center bg-[white] border-2 ${dayjs(date).format("YYYY-MM-DD") === dayjs(currentDate).format('YYYY-MM-DD') ? 'border-[#2564C0]':''} rounded relative `}>
               {workoutDays.find(d => dayjs(d.date).format('MM DD YYYY') === date.format('MM DD YYYY')) ? <div className='w-2 h-2 bg-[green] absolute top-2 right-2 rounded-lg'/> : null}
               <div onClick={(e) => {
                 setSelectedDate({ standard: dayjs(date), display: dayjs(date).format("YYYY-MM-DD") })
-              }} className={`text-black p-4 rounded-md mb-2`}>
+              }} className={`text-black p-4 mb-2 text-[12px]`}>
                 {date.format('MMM D')}
               </div>
             </div>
           )
         })}
       </div>
-      <Fab sx={{ position: "absolute", bottom: 50, right: 20 }} onClick={() => setShowForm(true)} color="primary" aria-label="add">
+      <Fab sx={{ position: "absolute", bottom: 100, right: 20 }} onClick={() => setShowForm(true)} color="primary" aria-label="add">
         <AddIcon />
       </Fab>
       <WorkoutList isFetchingWorkouts={isFetchingWorkouts} deleteExercise={removeExercise} datalist={datalist} selectedDate={selectedDate} />
