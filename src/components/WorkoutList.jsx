4e387cf71ee651d2e5e38cb6e85ffb6d429f73e0
developer-tvc <developer@techversantinfo.com>
2024-02-16 10:05:50 +0530

    sets and reps style change

--- src/components/WorkoutList.jsx
+++ src/components/WorkoutList.jsx
@@ -14,14 +14,14 @@ const WorkoutList = ({ datalist, deleteExercise, isFetchingWorkouts }) => {
         return <SmallLoader />
     }
 
-    if(!workoutFortheDay){
+    if (!workoutFortheDay) {
         return <div className="grid place-content-center pt-20">
-             <div className="text-sm font-normal text-[tomato]">No workout logged</div>
+            <div className="text-sm font-normal text-[tomato]">No workout logged</div>
         </div>
     }
 
     return (
-        <div style={{ padding: 12 }}> {workoutFortheDay?.exercises?.map(i => <div>
+        <div style={{ padding: 12 , paddingLeft:4}}> {workoutFortheDay?.exercises?.map(i => <div>
             <ListItemButton sx={{
                 '& .css-elzd3y-MuiButtonBase-root-MuiListItemButton-root': {
                     background: "red !important"
@@ -34,16 +34,27 @@ const WorkoutList = ({ datalist, deleteExercise, isFetchingWorkouts }) => {
                 {open === i._id ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
             <Collapse sx={{ position: "relative" }} in={open === i._id} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    {i.sets.map(set => <ListItemButton sx={{ pl: 4 }}>
+                    {/* {i.sets.map(set => <ListItemButton sx={{ pl: 4 }}>
                         <ListItemIcon>
                             <StarBorder />
                         </ListItemIcon>
                         <ListItemText primary={`${set.weight} kg`} />
                         <ListItemText primary={'x'} />
                         <ListItemText primary={`${set.reps} reps`} />
-                    </ListItemButton>)}
-                </List>
+                    </ListItemButton>)} */}
+                    <div className="flex pl-8">
+                    
+                    {i.sets.map(set => <div className="border-2 ">
+                        <div className="border-b-2 text-sm p-1">
+                            {set.weight} kg
+                        </div>
+                        <div className="text-sm font-normal p-1">
+                            {set.reps} reps
+                        </div>
+                    </div>
+                    )}
+                    </div>
+                   
                 <div style={{ position: "absolute", right: "20px", top: "10px", width: "20px", height: "20px" }} onClick={() => deleteExercise(workoutFortheDay, i._id)} >
                     <Delete color='tomato' />
                 </div>
