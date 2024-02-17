224adf8d7c690cd6bd465bcfb0e842c312115384
developer-tvc <developer@techversantinfo.com>
2024-02-17 14:35:22 +0530

    calender view workout number

--- src/components/CalenderView.jsx
+++ src/components/CalenderView.jsx
@@ -6,19 +6,26 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import ServerDay from './CalenderHighlightedDay';
 import { useQuery } from 'react-query';
-import { getWorkoutDays } from '../apis/workoutApis';
+import { getWorkoutDays, getWorkoutsByDate } from '../apis/workoutApis';
 import SmallLoader from './SmallLoader';
 
 const CalenderView = () => {
+  const [selectedDate, setSelectedDate] = useState({ standard: dayjs(), display: dayjs().format("YYYY-MM-DD") });
+
   const {
     isLoading: isFetchingWorkoutDays,
     data: workoutDays
   } = useQuery('workoutDays', getWorkoutDays)
 
+  const {
+    isLoading: isFetchingWorkouts,
+    data: workouts,
+  } = useQuery(['workouts', selectedDate.display], () => getWorkoutsByDate(selectedDate.display), {
+    enabled: selectedDate.display !== '',
+  });
+
+
   const highlightedDays = workoutDays?.map(i => dayjs(i.date).format('YYYY-MM-DD'))
-
-  console.log(highlightedDays, 'workout Days')
-
   const currentDate = new Date()
 
   if(isFetchingWorkoutDays){
@@ -31,8 +38,8 @@ const CalenderView = () => {
           variant='static'
           orientation='portrait'
           value={dayjs(currentDate)}
-          //  onChange={(e)=>{setSelectedDate({standard:dayjs(e.$d),display: dayjs(e.$d).format("DD-MM-YYYY")})
-          // }}
+           onChange={(e)=>{setSelectedDate({standard:dayjs(e.$d),display: dayjs(e.$d).format("YYYY-MM-DD")})
+          }}
           renderInput={(params) => {
             <TextField {...params} />
           }}
@@ -40,12 +47,19 @@ const CalenderView = () => {
             day: ServerDay,
           }}
           slotProps={{
+            actionBar:{actions:[]},
             day: {
               highlightedDays,
             },
           }}
         />
       </LocalizationProvider>
+      <div>
+        {workouts?.exercises?.length > 0 ?  <div>
+        <p className="text-sm">Workouts done : {workouts?.exercises?.length}</p>
+        <button  className='text-[blue] text-sm '>View more details</button>
+        </div>: <p className="text-sm">No workouts logged for this day</p>}
+      </div>
     </div>
   )
 }
